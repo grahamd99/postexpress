a1860e872ed6290ad0ae2da3e378c1ab6fbd50bb
Graham Dodsworth <graham.dodsworth@nhs.net>
2021-01-09 15:34:11 +0000

    Add patch and delete forms and delete endpoint

--- app.js
+++ app.js
@@ -4,16 +4,17 @@ const app = express()
 const port = 3000
 const path = require('path')
 const { v4: uuidv4 } = require('uuid')
+const methodOverride = require('method-override')
 
 // for parsing application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true })) 
 // for parsing application/json
 app.use(express.json()) 
-
+app.use(methodOverride('_method'))
 app.set('views', path.join(__dirname,'views'))
 app.set('view engine', 'ejs')
 
-const comments = [
+let comments = [
 	{
 		id: uuidv4(),
 		username: 'Todd',
@@ -34,7 +35,6 @@ const comments = [
 console.log(comments)
 
 app.get('/comments', (req, res) => {
-	  console.log("GET response for /comments")
   res.render('comments/index', {comments})
 })
 
@@ -54,15 +54,28 @@ app.patch('/comments/:id', (req, res) => {
   const newCommentText = req.body.comment
   const foundComment = comments.find(c => c.id === id)
   foundComment.comment = newCommentText
-  res.send('pacthing something')
   res.redirect('/comments')
 })
+
+app.get('/comments/:id/edit', (req, res) => {
+  const {id} = req.params
+  const comment = comments.find(c => c.id === id)
+  res.render('comments/edit', {comment})
+})
+
 
 app.post('/comments', (req, res) => {
   const { username, comment } = req.body
   comments.push( { username, comment, id: uuidv4()  } )
   res.redirect('/comments')
 })
+
+app.delete('/comments/:id', (req, res) => {
+  const {id} = req.params
+  comments = comments.filter(c => c.id !== id)
+  res.redirect('/comments')
+})
+
 
 app.get('/tacos', (req, res) => {
   res.send('GET tacos reponse')
